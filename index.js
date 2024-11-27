f8b50cb45aa673cbcd60a0201c456d4c4dc88117
jeedi-anuradha <radhakrishna3859@gmail.com>
2024-11-27 10:15:22 +0530

    for loop

---
+++ index.js
@@ -0,0 +1,47 @@
+/**1.Write a for loop to print the numbers from 1 to 20. */
+for(var i=1;i<=20;i++){
+    console.log(i);
+}
+/**2.Create a for loop to print the squares of numbers from 1 to 10. */
+for(var i=1;i<=10;i++){
+    console.log(i*i);
+}
+/**3.Use a for loop to print the first 10 multiples of 5. */
+for(var i=1;i<=10;i++){
+    console.log(i*5);
+}
+/**4.Write a for loop to print numbers from 20 to 1 in reverse order. */
+for(var i=20;i>0;i--){
+    console.log(i);
+}
+/**5.Create a for loop to print the first 10 even numbers in reverse order (e.g., 20, 18, 16, …). */
+for(i=20;i>=2;i--){
+    if(i%2==0){
+        console.log(i);
+    }   
+} 
+/**6.Print each element of the array
+Input:[10, 20, 30, 40, 50]
+
+Expected Output:
+10  
+20  
+30  
+40  
+50*/
+var arr=[10,20,30,40,50];
+for(var i=0;i<=4;i++){
+    console.log(arr[i]);
+} 
+
+/**7.Print the index of each element along with its value
+–Input: ['apple', 'banana', 'cherry']
+
+–Expected Output:
+Index 0: apple  
+Index 1: banana  
+Index 2: cherry   */
+var arr=['apple','banana','cherry'];
+for(var i=0;i<=2;i++){
+    console.log(`Index ${i} : ${arr[i]}`)
+}
